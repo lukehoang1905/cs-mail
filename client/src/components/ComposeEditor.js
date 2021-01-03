762b1d11c51dea93d01fad124e9bae0031b951d4
luke <lukehoang195@gmail.com>
2021-01-03 15:15:24 +0700

    Composer update

--- client/src/components/ComposeEditor.js
+++ client/src/components/ComposeEditor.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { InputBase, Paper } from "@material-ui/core";
+import { Box, InputBase, Paper } from "@material-ui/core";
+import { styled } from '@material-ui/core/styles';
+
+const StyledHeader = styled(Box)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  padding: '0.3rem',
+  width:"20vw"
+});
 
 const useStyles = makeStyles((theme) => ({
   root: { display: "flex", height: "60vh", flexDirection: "column" },
@@ -12,13 +23,14 @@ const ComposeEditor = () => {
   return (
     <>
       <Paper elevation={3} className={classes.root}>
-        <div>New Message</div>
-        <div>To</div>
-        <div>Subject</div>
+        <StyledHeader>New Message</StyledHeader>
+        <InputBase placeholder="To : "/>
+        <InputBase placeholder="Subject : "/>
         <InputBase
           id="outlined-multiline-static"
           multiline
           className={classes.textField}
+          placeholder="Content : "
         />
         <div>Icon</div>
       </Paper>
